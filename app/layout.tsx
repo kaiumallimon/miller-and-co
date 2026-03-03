6a92369eb06a935802a40880434d4df7ba037ded
Kaium Al Limon <limon.3cs@gmail.com>
2026-03-03 08:24:56 +0600

    Add suppressHydrationWarning to html tag in RootLayout for improved hydration handling

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       {/* Prevent flash of content before preloader mounts on first visit */}
       <head>
         <script dangerouslySetInnerHTML={{ __html: `
